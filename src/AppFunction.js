c355b63769450084db58782e3df519d213c4e01b
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-04-26 13:33:49 -0700

    intro to useState and useEffect

---
+++ src/AppFunction.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
+    const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+    useEffect(() => {
+        document.title = `You have clicked ${count} times`;
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        }
+    }, [count]);
+
+    const handleMouseMove = event => {
+        setMousePosition({
+            x: event.pageX,
+            y: event.pageY,
+        });
+    }
+
+    const incrementCount = () => {
+        setCount( prevCount => prevCount + 1 );
+    };
+
+    const toggleLight = () => {
+        setIsOn( prevIsOn => !prevIsOn)
+    }
+
+    return (
+        <>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>
+                I was clicked {count} times
+            </button>
+
+            <h2>Toggle Light</h2>
+            <img
+                src={
+                    isOn 
+                    ? 'https://icon.now.sh/highlight/fd0'
+                    : 'https://icon.now.sh/highlight/aaa'
+                }
+                style={{
+                height: '50px',
+                width: '50px',
+                }}
+                alt="Flashlight"
+                onClick={toggleLight}
+            />
+
+            <h2>Mouse Position</h2>
+            <p>X position: {mousePosition.x}</p>
+            <p>Y position: {mousePosition.y}</p>
+            <br />
+
+        </>
+    );
+}
+
+export default App;
