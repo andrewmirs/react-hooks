975cb899040151f5c3aa323a3c22a43ef0158b17
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-04-28 16:01:10 -0700

    using useEffect to monitor when components are mounted and unmounted

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,18 +1,54 @@
 import React, { useState, useEffect } from 'react';
 
+const initialLocationState = {
+    latitude: null,
+    longitude: null,
+    speed: null,
+}
+
 const App = () => {
     const [count, setCount] = useState(0);
     const [isOn, setIsOn] = useState(false);
     const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+    const [status, setStatus] = useState(navigator.onLine);
+    const [{ latitude, longitude, speed }, setLocation] = useState(initialLocationState);
+    let mounted = true;
 
     useEffect(() => {
         document.title = `You have clicked ${count} times`;
         window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+        navigator.geolocation.getCurrentPosition(handleGeolocation);
+        const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+            navigator.geolocation.clearWatch(watchId);
+            mounted = false;
         }
     }, [count]);
+
+    const handleOnline = () => {
+        setStatus(true);
+    }
+
+    const handleOffline = () => {
+        setStatus(false);
+    }
+
+    const handleGeolocation = (event) => {
+        if (mounted){
+            setLocation({
+                latitude: event.coords.latitude,
+                longitude: event.coords.longitude,
+                speed: event.coords.speed,
+            });
+        }
+        return
+    }
 
     const handleMouseMove = event => {
         setMousePosition({
@@ -55,7 +91,14 @@ const App = () => {
             <p>X position: {mousePosition.x}</p>
             <p>Y position: {mousePosition.y}</p>
             <br />
+            
+            <h2>Network Status</h2>
+            <p>You are <strong>{status ? 'online' : 'offline'}</strong></p>
 
+            <h2>Geolocation</h2>
+            <p>Latitude is {latitude ? latitude : 'loading..'}</p>
+            <p>longitude is {longitude ? longitude : 'loading..'}</p>
+            <p>Your speed is {speed ? speed : '0'}</p>
         </>
     );
 }
